f84666277ccc6f25d492638955ebf144c40371ef
ryoma kawahara <akahirefriend2008@yahoo.com>
2018-07-15 11:38:58 +0900

    test usage for single file components

---
+++ app/javascript/packs/test.js
@@ -0,0 +1,9 @@
+import Vue from "vue"
+import Test from "../test.vue";
+
+new Vue({
+  el: '#test',
+  components: {
+    'test': Test
+  },
+});
---
+++ app/javascript/test.vue
@@ -0,0 +1,19 @@
+<template>
+  <p>this is from a single file component! {{ message }}</p>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: "hello"
+    };
+  }
+};
+</script>
+
+<style scoped>
+p {
+  color: red;
+}
+</style>
