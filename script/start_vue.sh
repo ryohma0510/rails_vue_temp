addb73cfde4b27c7777b91c1c36f8aa5695fc20b
ryoma kawahara <akahirefriend2008@yahoo.com>
2018-07-15 23:35:50 +0900

    modify script

--- script/start_vue.sh
+++ script/start_vue.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker-compose run web yarn install
 docker-compose run web rails webpacker:install:vue
-docker-compose run web rails rails db:create
+docker-compose run web rails db:create
